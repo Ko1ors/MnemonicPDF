043d50e52a76951edb3894a5dc71bb91ebadfd7a
Ko1ors <kolorsnik181200@gmail.com>
2021-05-17 22:45:39 +0300

    Color picker added

---
+++ Mnemonic-phrase-to-PDF/Converters/BrushToColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Mnemonic_phrase_to_PDF.Converters
+{
+    public class BrushToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is SolidColorBrush)
+            {
+                var brush = value as SolidColorBrush;
+                return brush.Color;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Color)
+                return new SolidColorBrush((Color)value);
+            return null;
+        }
+    }
+}
